e73f644050b47cddc02f274b97909db4f98c37b2
Liratanak <annliratanak@gmail.com>
2014-08-16 20:13:20 +0700

    custom class loader

--- src/nfh/speeder/sample/android/customloaddexfile/MainActivity.java
+++ src/nfh/speeder/sample/android/customloaddexfile/MainActivity.java
@@ -1,24 +1,57 @@
 package nfh.speeder.sample.android.customloaddexfile;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
+import dalvik.system.DexClassLoader;
 
+@SuppressLint("NewApi")
 public class MainActivity extends Activity {
 
 	TextView helloTextView;
-	
+	DexClassLoader dexClassLoader;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
+		CustomizedDexClassLoader.setContext(this);
+
+		try {
+			dexClassLoader = CustomizedDexClassLoader.load("hello.dex");
+		} catch (RuntimeException e) {
+			throw new RuntimeException(e);
+		}
+
 		helloTextView = (TextView) findViewById(R.id.textview);
 	}
-	
+
 	@Override
 	protected void onStart() {
 		super.onStart();
-		helloTextView.setText("Hello " + new nfh.speeder.sample.gradle.hello.Word().speeder());
+
+		try {
+
+			Class<?> wordClass = dexClassLoader.loadClass("nfh.speeder.sample.gradle.hello.Word");
+			Method speederMethod = wordClass.getMethod("speeder");
+			String speeder = (String) speederMethod.invoke(wordClass.newInstance());
+			helloTextView.setText("Hello " + speeder);
+
+			//The above 4 lines equals below
+			//helloTextView.setText("Hello " + new nfh.speeder.sample.gradle.hello.Word().speeder());
+
+		} catch (ClassNotFoundException e) {
+		} catch (NoSuchMethodException e) {
+		} catch (IllegalAccessException e) {
+		} catch (IllegalArgumentException e) {
+		} catch (InvocationTargetException e) {
+		} catch (InstantiationException e) {
+		}
+
 	}
 }
